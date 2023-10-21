bcd734c60356c9b3c17f1214ae42cf21d097366f
allen <allenshaw19@gmail.com>
2023-10-22 00:04:47 +0800

    add

--- leetcode/array/maxsubarray.go
+++ leetcode/array/maxsubarray.go
@@ -1,5 +1,31 @@
 package array
 
+import "math"
+
 func maxSubArray(nums []int) int {
-	panic("no implement")
+	window, maxSum := 0, math.MinInt
+
+	left, right := 0, 0
+
+	for right < len(nums) {
+		num := nums[right]
+		right++
+
+		window += num
+		maxSum = max(maxSum, window)
+
+		for left < right && window < 0 {
+			window -= nums[left]
+			left++
+		}
+	}
+
+	return maxSum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- leetcode/array/maxsubarray_test.go
+++ leetcode/array/maxsubarray_test.go
@@ -1,7 +1,24 @@
 package array
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMaxSubArray(t *testing.T) {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	ans := maxSubArray(nums)
+	fmt.Println(ans)
+}
 
+func TestMaxSubArray2(t *testing.T) {
+	nums := []int{1}
+	ans := maxSubArray(nums)
+	fmt.Println(ans)
+}
+
+func TestMaxSubArray3(t *testing.T) {
+	nums := []int{5, 4, -1, 7, 8}
+	ans := maxSubArray(nums)
+	fmt.Println(ans)
 }
