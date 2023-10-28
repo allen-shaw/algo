1789545890b1db742f0bb613508378258739ff04
allen <allenshaw19@gmail.com>
2023-10-28 18:46:48 +0800

    add

--- leetcode/stack/decodestring.go
+++ leetcode/stack/decodestring.go
@@ -1,13 +1,50 @@
 package stack
 
+type pair struct {
+	count int
+	str   []byte
+}
+
 func decodeString(s string) string {
-	// ans := make([]byte, 0)
-	// for i := 0; i < len(s)-1; {
-	// 	b := s[i]
-	// 	if
+	count := 0
+	stack := NewStack[pair]()
+	ans := make([]byte, 0)
 
-	// }
+	for i := 0; i < len(s); i++ {
+		c := s[i]
 
-	// return string(ans)
-	panic("no implement")
+		if isNum(c) {
+			count = 10*count + ctoNum(c)
+		} else if c == '[' {
+			stack.Push(pair{count: count, str: ans})
+			count = 0
+			ans = make([]byte, 0)
+		} else if c == ']' {
+			p := stack.Pop()
+			ss := buildStr(p.count, ans)
+			ans = append(p.str, ss...)
+		} else {
+			ans = append(ans, c)
+		}
+	}
+	return string(ans)
+}
+
+func isNum(c byte) bool {
+	if c >= '0' && c <= '9' {
+		return true
+	}
+	return false
+}
+
+func ctoNum(c byte) int {
+	return int(c - '0')
+}
+
+func buildStr(num int, ss []byte) []byte {
+	out := make([]byte, 0, num*len(ss))
+	for i := 0; i < num; i++ {
+		out = append(out, ss...)
+	}
+	return out
 }
--- leetcode/stack/decodestring_test.go
+++ leetcode/stack/decodestring_test.go
@@ -1,1 +1,40 @@
 package stack
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsNum(t *testing.T) {
+	a := byte('a')
+	fmt.Println(a, " is a num ", isNum(a))
+
+	b := byte('1')
+	fmt.Println(b, " is a num ", isNum(b))
+}
+
+func TestCToNum(t *testing.T) {
+	a := byte('2')
+	fmt.Println(a, " to num ", ctoNum(a))
+}
+
+func TestDecodeString(t *testing.T) {
+	s := "3[a]2[bc]"
+	ans := decodeString(s)
+	fmt.Println(ans)
+	// aaabcbc
+}
+
+func TestDecodeString2(t *testing.T) {
+	s := "3[a2[c]]"
+	ans := decodeString(s)
+	fmt.Println(ans)
+	// accaccacc
+}
+
+func TestDecodeString3(t *testing.T) {
+	s := "2[abc]3[cd]ef"
+	ans := decodeString(s)
+	fmt.Println(ans)
+	// abcabccdcdcdef
+}
