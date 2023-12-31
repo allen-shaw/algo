49c9bb3dde3fdbb27c6dfd062041803192ef6179
allen <allenshaw19@gmail.com>
2024-01-01 00:52:17 +0800

    feat(add)

--- graph/dfs.go
+++ graph/dfs.go
@@ -1,5 +1,38 @@
 package graph
 
+import "fmt"
+
+var dir = []int{-1, 0, 1, 0, -1}
+
 func areaRound(board [][]byte) {
-	
+	m, n := len(board), len(board[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if (i == 0 || j == 0 || i == m-1 || j == n-1) && board[i][j] == 'O' {
+				dfs(board, i, j)
+			}
+		}
+	}
+
+	fmt.Println(board)
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if board[i][j] == '#' {
+				board[i][j] = 'O'
+			} else if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			}
+		}
+	}
+}
+
+func dfs(board [][]byte, i, j int) {
+	board[i][j] = '#'
+	for k := 0; k < 4; k++ {
+		x, y := i+dir[k], j+dir[k+1]
+		if x >= 0 && x < len(board) && y >= 0 && y < len(board[0]) && board[x][y] == 'O' {
+			dfs(board, x, y)
+		}
+	}
 }
--- graph/dfs_test.go
+++ graph/dfs_test.go
@@ -1,7 +1,30 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAreaRound(t *testing.T) {
+	var testset = []struct {
+		in  [][]byte
+		out [][]byte
+	}{
+		// {
+		// 	[][]byte{{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}},
+		// 	[][]byte{{'X', 'X', 'X', 'X'}, {'X', 'X', 'X', 'X'}, {'X', 'X', 'X', 'X'}, {'X', 'O', 'X', 'X'}},
+		// },
+		{
+			[][]byte{{'X', 'O', 'X'}, {'X', 'O', 'X'}, {'X', 'O', 'X'}},
+			[][]byte{{'X', 'O', 'X'}, {'X', 'O', 'X'}, {'X', 'O', 'X'}},
+		},
+	}
 
+	for _, tt := range testset {
+		t.Run("areaRound", func(t *testing.T) {
+			areaRound(tt.in)
+			assert.Equal(t, tt.out, tt.in)
+		})
+	}
 }
