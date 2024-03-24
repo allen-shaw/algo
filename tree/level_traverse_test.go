6941d2d08b3a007a75bd206556d9e2718dba4540
Allen <allenshaw19@gmail.com>
2024-03-24 21:02:16 +0800

    add

--- tree/level_traverse_test.go
+++ tree/level_traverse_test.go
@@ -150,3 +150,149 @@ func Test_MAryToBinaryTree(t *testing.T) {
 	printNTree(root)
 
 }
+
+func distanceK(root *TreeNode, target *TreeNode, k int) []int {
+	m := make(map[*TreeNode]*TreeNode) // child -> parent
+
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		if root.Left != nil {
+			dfs(root.Left)
+			m[root.Left] = root
+		}
+		if root.Right != nil {
+			dfs(root.Right)
+			m[root.Right] = root
+		}
+	}
+
+	dfs(root)
+
+	q := []*TreeNode{target}
+	lvl := 0
+	ans := make([]int, 0)
+	visited := make(map[*TreeNode]bool)
+
+	for len(q) > 0 {
+		size := len(q)
+		for i := 0; i < size; i++ {
+			n := q[0]
+			q = q[1:]
+
+			if visited[n] {
+				continue
+			}
+			visited[n] = true
+
+			if lvl == k {
+				ans = append(ans, n.Val)
+			}
+			if lvl > k {
+				return ans
+			}
+
+			if n.Left != nil {
+				q = append(q, n.Left)
+			}
+			if n.Right != nil {
+				q = append(q, n.Right)
+			}
+			if p, ok := m[n]; ok {
+				q = append(q, p)
+			}
+		}
+
+		lvl++
+	}
+	return ans
+}
+
+func findClosestLeaf(root *TreeNode, k int) int {
+	parents := make(map[*TreeNode]*TreeNode)
+	var kNode *TreeNode
+
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		if root.Val == k {
+			kNode = root
+		}
+
+		if root.Left != nil {
+			dfs(root.Left)
+			parents[root.Left] = root
+		}
+		if root.Right != nil {
+			dfs(root.Right)
+			parents[root.Right] = root
+		}
+	}
+	visited := make(map[*TreeNode]bool)
+	q := []*TreeNode{kNode}
+	for len(q) > 0 {
+		size := len(q)
+		for i := 0; i < size; i++ {
+			n := q[0]
+			q = q[1:]
+
+			if n.Left == nil && n.Right == nil {
+				return n.Val
+			}
+
+			if visited[n] {
+				continue
+			}
+			visited[n] = true
+
+			if n.Left != nil {
+				q = append(q, n.Left)
+			}
+			if n.Right != nil {
+				q = append(q, n.Right)
+			}
+			if p, ok := parents[n]; ok {
+				q = append(q, p)
+			}
+		}
+	}
+
+	return 0
+}
+
+func killProcess(pid, ppid []int, kill int) []int {
+	pids := make(map[int][]int, 0)
+
+	for i, parentid := range ppid {
+		processid := pid[i]
+		pids[parentid] = append(pids[parentid], processid)
+	}
+
+	ans := make([]int, 0)
+	q := []int{kill}
+	for len(q) > 0 {
+		p := q[0]
+		q = q[1:]
+
+		ans = append(ans, p)
+		if len(pids[p]) > 0 {
+			q = append(q, pids[p]...)
+		}
+	}
+
+	return ans
+}
+
+func Test_killProcess(t *testing.T) {
+	pid := []int{1, 3, 10, 5}
+	ppid := []int{3, 0, 5, 3}
+	kill := 5
+
+	ans := killProcess(pid, ppid, kill)
+	fmt.Println(ans)
+}
